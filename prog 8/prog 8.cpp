4ef21b5ccd5306f949b7a59988095a004ce958df
EugeneFreak <zeny-medved@mail.ru>
2022-05-14 18:15:27 +0300

    Добавьте файлы проекта.

---
+++ prog 8/prog 8.cpp
@@ -0,0 +1,34 @@
+﻿//kalculator
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    double x, y;
+    string z;
+    cin >> x;
+    cin >> z;
+    cin >> y;
+    if (z == "+") {
+        cout << "= " << x + y << endl;
+    }
+    else if (z == "-") {
+        cout << "= " << x - y << endl;
+    }
+    else if (z == "*") {
+        cout << "= " << x * y << endl;
+    }
+    else if (z == "/" && y == 0) {
+        cout << "Division by zero" << endl;
+    }
+    else if (z == "/") {
+        cout << "= " << x / y << endl;
+    }
+    else {
+        cout << "Unknown operation: " << z << endl;
+    }
+}
